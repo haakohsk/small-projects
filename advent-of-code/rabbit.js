b076308de3e8a87e8faf3193873ffa60cce528eb
Håkon Skaufel <hakonskaufel@dhcp-10-22-3-112.wlan.ntnu.no>
2021-12-03 11:44:43 +0100

    add c-prog dir

---
+++ advent-of-code/rabbit.js
@@ -0,0 +1,16 @@
+// Du har 100 kanin-hull
+// en kanin befinner seg i ett av hullene
+// du kan bare sjekke ett hull av gangen, men kan sjekke så mange hull som trengs for å finne kaninen
+// og kaninen hopper alltid til et av sine nabohull
+// -> dvs. hvis kanin starter på 50 kan den hoppe til 49 eller 51 
+// kaninen hopper 1 gang for hvert hull du sjekker
+
+// hvordan er man garantert å alltid finne kaninen??
+
+// [- - - x]
+//  0 1 2 3
+
+// + - - x - -
+// - + x - - -
+// - x + - - -
+// hint 1: - hva vet du hvis kaninen starter i hull eks. 50?
